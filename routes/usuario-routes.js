c7745e8867ca3e1e7c54d96a783a767c21ea74b7
Rodrigo Caetano <-c>
2021-12-13 07:44:08 -0300

    removido controller e finalizado crud

--- routes/usuario-routes.js
+++ routes/usuario-routes.js
@@ -13,8 +13,42 @@ router.post('/users', (req, res) => {
 });
 
 
+//buscar usuário
+router.get('/users', (req, res) => {
+    userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }))
+});
+
+//buscar um cliente especifico 
+router.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }))
+});
+
+//editar um cliente especifico 
+router.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, sobrenome, email } = req.body;
+    userSchema
+    .updateOne({_id: id }, { $set: { nome, sobrenome, email }})
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }))
+});
 
 
+//deletar um cliente especifico 
+router.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    userSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }))
+});
 
 
 module.exports = router;
